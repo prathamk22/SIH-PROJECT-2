2d6a2f10e9b41401f8b72aaf852686c7b43d9cb1
prathamkhurana <pratham@appflixstudio.com>
2022-08-20 18:50:23 +0530

    Added account profile and getting data

--- app/src/main/java/com/kanyideveloper/firebasecoroutinesdemo/ui/home/ui/account/AccountFragment.kt
+++ app/src/main/java/com/kanyideveloper/firebasecoroutinesdemo/ui/home/ui/account/AccountFragment.kt
@@ -1,32 +1,54 @@
 package com.kanyideveloper.firebasecoroutinesdemo.ui.home.ui.account
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.kanyideveloper.firebasecoroutinesdemo.R
+import com.kanyideveloper.firebasecoroutinesdemo.databinding.FragmentAccountBinding
+import com.kanyideveloper.firebasecoroutinesdemo.databinding.FragmentNotificationsBinding
+import com.kanyideveloper.firebasecoroutinesdemo.ui.LoginActivity
+import com.kanyideveloper.firebasecoroutinesdemo.ui.home.HomeActivity
+import com.kanyideveloper.firebasecoroutinesdemo.util.PreferenceHelper
 
 class AccountFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AccountFragment()
-    }
+    private val viewModel: AccountViewModel by viewModels()
 
-    private lateinit var viewModel: AccountViewModel
+    private var _binding: FragmentAccountBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater)
+        return _binding!!.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.user.observe(viewLifecycleOwner) {
+            binding.personName.text = it.name
+            binding.personEmail.text = it.email
+        }
+        binding.logout.setOnClickListener {
+            PreferenceHelper.userId = ""
+            PreferenceHelper.isLoggedIn = false
+            startActivity(Intent(requireContext(), LoginActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            })
+            requireActivity().finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/kanyideveloper/firebasecoroutinesdemo/ui/home/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/kanyideveloper/firebasecoroutinesdemo/ui/home/ui/account/AccountViewModel.kt
@@ -1,7 +1,25 @@
 package com.kanyideveloper.firebasecoroutinesdemo.ui.home.ui.account
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kanyideveloper.firebasecoroutinesdemo.model.User
+import kotlinx.coroutines.launch
 
-class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AccountViewModel(
+    private val repository: AccountRepository = AccountRepository()
+) : ViewModel() {
+
+    val user = MutableLiveData<User>()
+
+    init {
+        getUser()
+    }
+
+    private fun getUser() {
+        viewModelScope.launch {
+            user.postValue(repository.getCurrentUser())
+        }
+    }
+
 }
